16d25a74135ded4d88b894692e46c8dce76d4974
woo9599 <86222066+woo9599@users.noreply.github.com>
2021-06-21 23:59:50 +0900

    Delete python/python_0510 directory

--- python/python_0510/python02_niceday_우상민.py
+++
@@ -1,5 +0,0 @@
-print('nice day')
-print('nice day')
-vo01 = 10
-vo02 = 20
-print('vo01+vo02')
--- python/python_0510/python06_ifex02and_우상민.py
+++
@@ -1,6 +0,0 @@
-vld = 'orange'
-pwd = '1234'
-if vld == 'red' and pwd == '1234':
-    print('반갑습니다. 회원님')
-else:
-    print('회원확인하세요.')
